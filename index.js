23bc24618d99c36c1590984aabf983b92c260a00
okmanl <ericgumba@gmail.com>
2018-01-26 14:22:05 -0800

    checkpoint, got auth callback to work. It turns out that new GoogleStrategy is called when the route that has passport.authenticate('google') is requested.

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
+
+console.log(keys.googleClientID);
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback',
+}, accessToken => { console.log(accessToken) }));
+
 
 
 const app = express();
@@ -7,6 +18,20 @@ const app = express();
 app.get('/', (req, res) => {
     res.send('<p> some html </p>');
 });
+
+
+
+// set passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email'],
+}));
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
 
 
 app.listen(5000);
