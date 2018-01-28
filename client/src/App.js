59bea9e074205d641d56c1239113519143459e19
okmanl <ericgumba@gmail.com>
2018-01-27 17:12:17 -0800

    Finally got passport to work. Problem was using ternary operator in google strategy todo: refactor everything

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <a href="http://localhost:5000/auth/google">Sign in with Google</a>
+        <div></div>
+        <a href="http://localhost:5000/api/user"> check to see program remembers authentication</a>
       </div>
     );
   }
--- index.js
+++ index.js
@@ -2,23 +2,25 @@ const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('./config/keys');
+const mongoose = require('mongoose');
+require('./models/user'); // must be executed before User = mongoose.model('user')
+const User = mongoose.model('users');
+const cookie = require('cookie-session');
 
-
-console.log(keys.googleClientID);
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback',
-}, accessToken => { console.log(accessToken) }));
-
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI);
 
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('<p> some html </p>');
-});
 
+
+app.use(
+    cookie({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey],
+    })
+);
 
 
 // set passport middleware
@@ -26,11 +28,61 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 
+// PASSPORT 
+
+passport.serializeUser((user, done) => {
+    console.log("seralized")
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    console.log("deserialized")
+    User.findById(id).then(user => {
+        done(null, user);
+    });
+});
+
+
+passport.use(new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback',
+}, (accessToken, refreshToken, profile, done) => {
+    User.findOne({ googleId: profile.id }).then(
+        existingUser => {
+            // existingUser ? done(null, existingUser) :
+            //     new User({ googleId: profile.id }).save().then(
+            //         user => done(null, user)
+            //     )
+            if (existingUser) {
+                done(null, existingUser);
+            } else {
+                new User({ googleId: profile.id }).save().then(
+                    user => done(null, user)
+                )
+            }
+        }
+    )
+}));
+
+
+// ROUTING 
+
+app.get('/', (req, res) => {
+    res.send('<p> some html </p>');
+});
+
+
+
 app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email'],
+    scope: ['profile', 'email']
 }));
 
 app.get('/auth/google/callback', passport.authenticate('google'));
+
+app.get('/api/user', (req, res) => {
+    res.send(req.user);
+});
 
 
 
